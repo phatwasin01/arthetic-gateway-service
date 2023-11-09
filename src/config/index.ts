5d6fa3b78d8fd86ebe1c5d8f5a9ac99f561885e6
Phatwasin Suksai <phatwasin01@gmail.com>
2023-11-10 00:17:57 +0700

    delete follows type

--- src/config/index.ts
+++ src/config/index.ts
@@ -11,6 +11,7 @@ const envSchema = z.object({
   USER_SERVICE_URL: z.string().optional(),
   POST_SERVICE_URL: z.string().optional(),
   MARKET_SERVICE_URL: z.string().optional(),
+  CHAT_SERVICE_URL: z.string().optional(),
   NODE_ENV: z.string().optional(),
 });
 
--- src/index.ts
+++ src/index.ts
@@ -6,17 +6,43 @@ import {
 } from "@apollo/gateway";
 import { verifyJwtToken } from "./libs/auth";
 import { config } from "./config";
-
 import { expressMiddleware } from "@apollo/server/express4";
 import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
 import express from "express";
 import http from "http";
 import cors from "cors";
+import winston from "winston";
+const myFormat = winston.format.printf(
+  ({ level, message, label, timestamp }) => {
+    return `${timestamp} [${label}] ${level}: ${message}`;
+  }
+);
+// Configure the Winston logger
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.colorize(),
+    myFormat
+  ),
+});
 
+// Add Console transport if not in production
+if (process.env.NODE_ENV !== "production") {
+  logger.add(
+    new winston.transports.Console({
+      format: winston.format.simple(),
+    })
+  );
+}
+
+// Initialize Express
 const app = express();
 
+// Create HTTP server
 const httpServer = http.createServer(app);
 
+// Apollo Gateway setup
 const gateway = new ApolloGateway({
   supergraphSdl: new IntrospectAndCompose({
     subgraphs: [
@@ -26,13 +52,13 @@ const gateway = new ApolloGateway({
         name: "market",
         url: config.MARKET_SERVICE_URL || "http://market-svc:4000",
       },
+      // { name: "chat", url: config.CHAT_SERVICE_URL || "http://chat-svc:4000" },
     ],
   }),
   buildService({ url }) {
     return new RemoteGraphQLDataSource({
       url,
       willSendRequest({ request, context }) {
-        // the request variable is to sent to subgraphs
         const { token } = context;
         try {
           if (token && typeof token === "string") {
@@ -40,39 +66,71 @@ const gateway = new ApolloGateway({
             request?.http?.headers.set("user-id", decoded.id);
           }
         } catch (err) {
-          console.log("No jwt token found");
+          logger.warn("No valid token found");
         }
       },
     });
   },
 });
 
+// Apollo Server setup
 const server = new ApolloServer({
   gateway,
   nodeEnv: "development",
-  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  plugins: [
+    ApolloServerPluginDrainHttpServer({ httpServer }),
+    {
+      async serverWillStart() {
+        logger.info("Server starting up!");
+      },
+      async requestDidStart() {
+        // logger.info("Request started! Query");
+
+        return {
+          // Fires whenever Apollo Server will parse a GraphQL
+          // request to create its associated document AST.
+          async parsingDidStart() {
+            logger.info("Parsing started!");
+          },
+
+          // Fires whenever Apollo Server will validate a
+          // request's document AST against your GraphQL schema.
+          async validationDidStart() {
+            logger.info("Validation started!");
+          },
+        };
+      },
+    },
+  ],
 });
+
+// Start the server
 (async () => {
   await server.start();
+  app.use(cors<cors.CorsRequest>({ origin: "*" }));
+  app.use(express.json());
   app.use(
     "/graphql",
-    cors<cors.CorsRequest>({ origin: "*" }),
-    express.json(),
     expressMiddleware(server, {
       context: async ({ req }) => ({ token: req.headers.authorization }),
     })
   );
+
   app.get("/", (req, res) => {
     res.send("hello world");
   });
+
   app.get("/health", (req, res) => {
     res.send("ok");
   });
+
   app.get("/healthz", (req, res) => {
     res.send("ok");
   });
+
   await new Promise<void>((resolve) =>
     httpServer.listen({ port: config.PORT }, resolve)
   );
-  console.log(`🚀  Server ready at: ${config.PORT}`);
+
+  logger.info(`🚀 Server ready at http://localhost:${config.PORT}/graphql`);
 })();
